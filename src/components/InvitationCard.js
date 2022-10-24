37cf091fb6bdb8ce74b7d448d8a1c49ddcc74c8c
Rikirhenaldi <77678762+Rikirhenaldi@users.noreply.github.com>
2022-10-24 18:39:21 +0700

    revisi mobile section 1

--- src/components/InvitationCard.js
+++ src/components/InvitationCard.js
@@ -6,7 +6,11 @@ export default function InvitationCard() {
     return (
         <div className='cardInvitation'>
             <h1 className='titleCardMobile titleCard'>Pernikahan</h1>
-            <h2 className='coupleNameMobile coupleName'>Makbul & Riana</h2>
+            <h2 className='coupleNameMobile coupleName'>
+               <span>Makbul</span> 
+               <span>&</span> 
+                <span>Riana</span>
+                </h2>
             <div className='cardBoxRow'>
                 <div className='boxLeftMobile boxLeft'>
                     <h2>Mengundang Anda </h2>
