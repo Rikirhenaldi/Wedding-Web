6b37b39991bc67a3fdc691c232742d02dbaa8454
Rikirhenaldi <77678762+Rikirhenaldi@users.noreply.github.com>
2022-10-26 23:15:10 +0700

    section 3 add maps

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -51,23 +51,35 @@ function Home() {
         </div>
       </section>
       <section className='section3'>
-      <div className='beginingPart mb-6'>
+        <div className='beginingPart mb-6'>
           <h1 className='greeting'>Acara</h1>
           <div className='descripitionWrapper'>
-          <h2 className='descriptionFont16'>Kami Menyampaikan Kabar Bahagia Ini Dan Akan Lengkap Apabila Anda Berkenan Memberikan Doa Restu Kepada Kami Dalam Acara Pernikahan Kami</h2>
-          <h2 className='descriptionFont16 pt-4'>Berikut Ini :</h2>
+            <h2 className='descriptionFont16'>Kami Menyampaikan Kabar Bahagia Ini Dan Akan Lengkap Apabila Anda Berkenan Memberikan Doa Restu Kepada Kami Dalam Acara Pernikahan Kami</h2>
+            <h2 className='descriptionFont16 pt-4'>Berikut Ini :</h2>
           </div>
         </div>
         <div className='containerCardAcara'>
           <div className='cardAcaraWrapper'>
-            <CardAcara Acara={"Akad Nikah"} Hour={"09:00 - 11:00"} Date={'13 November 2022'}/>
+            <CardAcara Acara={"Akad Nikah"} Hour={"09:00 - 11:00"} Date={'13 November 2022'} />
           </div>
-          <div className='cardAcaraWrapper'>  
-            <CardAcara Acara={"Resepsi Pernikahan"} Hour={"11:00 - 15:00"} Date={'13 November 2022'}/>
+          <div className='cardAcaraWrapper'>
+            <CardAcara Acara={"Resepsi Pernikahan"} Hour={"11:00 - 15:00"} Date={'13 November 2022'} />
           </div>
         </div>
+        
+        <div className='beginingPart'>
+          <h1 className='greeting'>Lokasi Acara</h1>
+          <div className='descripitionWrapper'>
+            <h2 className='descriptionFont16'>Untuk memudahkan anda datang ketempat Acara diselenggarakan, silahkan Klik peta Google maps yang kami sediakan</h2>
+            <h2 className='descriptionFont16 pt-4'>Berikut Ini :</h2>
+          </div>
+        </div> 
+        <div className='mapWrapper'>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d494.962726483584!2d107.71020289731807!3d-7.044285398359859!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c11bb00b3b5b%3A0x126c492abda330a9!2sLapang%20Volly%20Iva%20Blue!5e0!3m2!1sid!2sid!4v1666797122682!5m2!1sid!2sid" width="100%" height="100%" style={{borderWidth: 2, borderRadius: 10,}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title='map'></iframe>
+        </div>
       </section>
-      <section className='section3'></section>
+      <section className='section4'>
+      </section>
     </div>
   )
 }
