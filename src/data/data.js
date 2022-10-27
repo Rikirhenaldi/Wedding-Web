e2391f9d5d56f2bd9439b59130f9fbccbcd30ee3
Rikirhenaldi <77678762+Rikirhenaldi@users.noreply.github.com>
2022-10-27 18:58:13 +0700

    section 4

---
+++ src/data/data.js
@@ -0,0 +1,59 @@
+export const ucapan = [
+    {
+        nama: "Sinta",
+        ucapan:" Alhamdulillah, semoga lancar sampe hari H😇 riri dan mbul"
+    },
+    {
+        nama: "Ketua Ormas",
+        ucapan:"Anjaaaayyy. Selamat Makbul dan pasangan!"
+    },
+    {
+        nama: "Cahya Aditya",
+        ucapan:"Mantap Makbul. selamat nyaa bul semoga SAMAWA. ingat bul, tong tarik teuing. Sakali deui Selamat ya Bul The Best lah."
+    },
+    {
+        nama: "Ust Gilang Gustiawan",
+        ucapan:"Alhamdulilah Mbul. Mugi janten keluarga anu sakinah mawadah sareng warahmah."
+    },
+    {
+        nama: "Rina",
+        ucapan:"Selamat riri. Mugi janten keluarga anu sakinah mawadah sareng warahmah."
+    },
+    {
+        nama: "Majid",
+        ucapan:"Alhamdulillah, yang ditunggu2 bul. Ikut bahagia🤩 semoga lancar sampe hari H menjadi keluarga samawa jodoh dunia akhirat aamiin 😇"
+    },
+    {
+        nama: "Dwi Ghia",
+        ucapan:"Aaaaa turut berbahagia sekali mendengar kabar ini. Barakallah teh Riri dan kang makbul, ngiring bingah, semoga menjadi keluarga yang sakinah mawadah warahmah, doakan shiftas yang lain diberikan jodoh terbaik di waktu dan dengan orang yang tepat 🤫☺️🙂🥰☁️🕊️"
+    },
+    {
+        nama: "Ferdy Sambo",
+        ucapan:" Alhamdulillah, semoga lancar sampe hari H😇 riri dan mbul"
+    },
+    {
+        nama: "Sumanto",
+        ucapan:"Anjaaaayyy. Selamat Makbul dan pasangan!"
+    },
+    {
+        nama: "Kevin",
+        ucapan:"Mantap Makbul. selamat nyaa bul semoga SAMAWA. ingat bul, tong tarik teuing. Sakali deui Selamat ya Bul The Best lah."
+    },
+    {
+        nama: "Abdul bugis",
+        ucapan:"Alhamdulilah Mbul. Mugi janten keluarga anu sakinah mawadah sareng warahmah."
+    },
+    {
+        nama: "Rina",
+        ucapan:"Selamat riri. Mugi janten keluarga anu sakinah mawadah sareng warahmah."
+    },
+    {
+        nama: "Majid",
+        ucapan:"Alhamdulillah, yang ditunggu2 bul. Ikut bahagia🤩 semoga lancar sampe hari H menjadi keluarga samawa jodoh dunia akhirat aamiin 😇"
+    },
+    {
+        nama: "Dwi Ghia",
+        ucapan:"Aaaaa turut berbahagia sekali mendengar kabar ini. Barakallah teh Riri dan kang makbul, ngiring bingah, semoga menjadi keluarga yang sakinah mawadah warahmah, doakan shiftas yang lain diberikan jodoh terbaik di waktu dan dengan orang yang tepat 🤫☺️🙂🥰☁️🕊️"
+    },
+    
+]
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,8 @@ import InvitationCard from '../components/InvitationCard'
 import CardAcara from '../components/CardAcara'
 import Makbul from '../assets/makbul.png'
 import Riana from '../assets/riana.png'
+import Heart from '../assets/heart.png'
+import { ucapan } from '../data/data'
 
 function Home() {
   return (
@@ -16,6 +18,7 @@ function Home() {
           <InvitationCard />
         </div>
       </section>
+      {/* <<<<<<<<< End of section 1 >>>>>>>>>>>>>>>>> */}
       <section className='section2'>
         <div className='beginingPart'>
           <h1 className='greeting'>Asslammualaikum</h1>
@@ -50,6 +53,7 @@ function Home() {
           </div>
         </div>
       </section>
+      {/* <<<<<<<<< End of section 2 >>>>>>>>>>>>>>>>> */}
       <section className='section3'>
         <div className='beginingPart mb-6'>
           <h1 className='greeting'>Acara</h1>
@@ -66,19 +70,55 @@ function Home() {
             <CardAcara Acara={"Resepsi Pernikahan"} Hour={"11:00 - 15:00"} Date={'13 November 2022'} />
           </div>
         </div>
-        
+
         <div className='beginingPart'>
           <h1 className='greeting'>Lokasi Acara</h1>
           <div className='descripitionWrapper'>
             <h2 className='descriptionFont16'>Untuk memudahkan anda datang ketempat Acara diselenggarakan, silahkan Klik peta Google maps yang kami sediakan</h2>
             <h2 className='descriptionFont16 pt-4'>Berikut Ini :</h2>
           </div>
-        </div> 
+        </div>
         <div className='mapWrapper'>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d494.962726483584!2d107.71020289731807!3d-7.044285398359859!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c11bb00b3b5b%3A0x126c492abda330a9!2sLapang%20Volly%20Iva%20Blue!5e0!3m2!1sid!2sid!4v1666797122682!5m2!1sid!2sid" width="100%" height="100%" style={{borderWidth: 2, borderRadius: 10,}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title='map'></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d494.962726483584!2d107.71020289731807!3d-7.044285398359859!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c11bb00b3b5b%3A0x126c492abda330a9!2sLapang%20Volly%20Iva%20Blue!5e0!3m2!1sid!2sid!4v1666797122682!5m2!1sid!2sid" width="100%" height="100%" style={{ borderWidth: 2, borderRadius: 10, }} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title='map'></iframe>
         </div>
       </section>
+      {/* <<<<<<<<< End of section 3 >>>>>>>>>>>>>>>>> */}
       <section className='section4'>
+        <div className='beginingPart'>
+          <h1 className='greeting'>Doa Dan Ucapan</h1>
+          <div className='descripitionWrapper'>
+            <h2 className='descriptionFont16'>Dari yang terkasih untuk kami:</h2>
+          </div>
+        </div>
+        <div className='boxScWrapper'>
+          <div className='scrollWrapper'>
+            {ucapan.map((item) => {
+              return (
+                <div className='listBox'>
+                  <div className='boxNama'>
+                    <img className='iconHeart' src={Heart} alt="heart" />
+                    <h1 className='name'>{item.nama}</h1>
+                    <h1>:</h1>
+                  </div>
+                  <div className='boxUcapan'>
+                    <h2 className='ucapan'>{item.ucapan}</h2>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </section>
+      {/* <<<<<<<<< End of section 4 >>>>>>>>>>>>>>>>> */}
+      <section className='section5'>
+        <div className='beginingPart'>
+          <h1 className='greeting'>Kirim Ucapan</h1>
+          <div className='descripitionWrapper'>
+            <h2 className='descriptionFont16'>Masukan ucapan dan doa kalian lewat form berikut:</h2>
+          </div>
+        </div>
+        {/* <!-- Load Facebook SDK for JavaScript --> */}
+        <div class="fb-comments" data-href="https://makbul-riana-undangan-nikah.netlify.app" data-width="100%" data-numposts="100"></div>
       </section>
     </div>
   )
